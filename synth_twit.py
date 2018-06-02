07a78f0366304cddf0e9253c3a225d54924c65e6
Stephen <stephen@umw.edu>
2018-06-01 22:35:31 -0400

    Actually write synthesized tweets to output file.

--- synth_twit.py
+++ synth_twit.py
@@ -5,6 +5,9 @@
 import numpy as np
 import pandas as pd
 import igraph as ig
+import datetime as dt
+import string
+import logging
 
 # Candidate features:
 #   Shortest path length
@@ -15,24 +18,55 @@ import igraph as ig
 #   Degree of intermediates
 #   Centralities
 
+try:
+    with open("/usr/share/dict/words") as w:
+        words = w.read().replace("'","").split('\n')
+        # (Just use 1/10th of the available words. Makes it run faster.)
+        words = np.random.choice(words, int(len(words)/10), replace=False)
+except:
+    words = ("now is the time for all good men " + \
+        "to come to the aid of their country").split(" ")
 
 # Write some number of days worth of activity to the file f.
-def record_n_days_worth_of_activity(g,num_days,num_node_activities_per_day,f):
+def record_n_days_worth_of_activity(g, num_days, num_node_activities_per_day,
+    start_date, f):
     for day in range(num_days):
+        start_date += dt.timedelta(days=1)
+        logging.info("Recording activity for {}...".format(
+            start_date.strftime("%b %d")))
         nodes = np.random.choice(range(g.vcount()),
             size=num_node_activities_per_day, replace=True)
         for node in nodes:
-            record_activity_for(g, node, f)
+            record_activity_for(g, node, start_date, f)
 
 # Write a line of text to the file f for a node of the graph g, containing a
 # tweet that (possibly) mentions one or more of his followees.
-def record_activity_for(g, node, f):
-    followees = g.neighbors(node, mode="out")
+def record_activity_for(g, node, date, f):
+    followees = g.neighbors(node, mode="in")
     if len(followees) > 0:
-        recipient = np.random.choice(followees,1)[0]
-        print("{} mentions {}".format(node,recipient))
+        recipients = np.random.choice(followees,np.random.randint(
+            0, len(followees)+1), replace=False)
+        tweet = gen_tweet(g, recipients)
+        logging.info("{} recipients, {} of which get tweeted to".format(
+            len(followees), len(recipients)))
+        logging.debug("{} mentions {}".format(node,
+            " and ".join([str(r) for r in recipients])))
+        print('{},{},"{}",{}'.format(
+            date.strftime("%a %b %d %H:%M:%S +0000 %Y"),
+            node+1000,tweet,'@'+g.vs[node]['name']), file=f)
     else:
-        print("{} has no one to mention".format(node))
+        logging.debug("{} has no one to mention".format(node))
+
+# Generate a random tweet, containing mentions of the recipients passed (if
+# any).
+def gen_tweet(g, recipients):
+    mentions = [ '@'+g.vs[recipient]['name'] for recipient in recipients ]
+    other_words = []
+    while sum([ len(w) + 1 for w in mentions + other_words]) < 140:
+        other_words += [np.random.choice(words,1)[0]]
+    tweet_words = mentions + other_words[:-1]
+    np.random.shuffle(tweet_words)
+    return ' '.join(tweet_words)
 
 
 # "Adder" functions return the probability of adding an edge between a node 
@@ -42,8 +76,8 @@ def record_activity_for(g, node, f):
 # An adder function that uses only the length of the shortest path (if any)
 # between the node and the potential neighbor. The mode argument controls
 # whether the shortest path must be directed or not (and if so, which way).
-def shortest_path_length_adder(g, node, potential_followee, mode="out"):
-    SHORTEST_PATH_CALIBRATION = .1
+def shortest_path_length_adder(g, node, potential_followee, mode="all"):
+    SHORTEST_PATH_CALIBRATION = .3
     sp = g.get_shortest_paths(node, potential_followee, mode=mode)[0]
     if len(sp) == 0:
         # Not connected at all. Return 0 (no chance of adding).
@@ -56,6 +90,7 @@ def shortest_path_length_adder(g, node, potential_followee, mode="out"):
 def synth_twit(
     num_nodes,         # The Erdos-Reyni n
     p,                 # The Erdos-Reyni p
+    screenname_length, # In chars after the "@"
     num_days_before,   # The number of simulated days before the 'epoch.'
     num_days_after,    # The number of simulated days after the 'epoch.'
     num_node_activities_per_day,  # The number of events (tweets, essentially)
@@ -65,18 +100,36 @@ def synth_twit(
     output_filename="tweets.csv", 
     plot=True):
 
+    saved_args = locals()
+
     with open(output_filename, "w") as f:
 
+        # Output paper trail.
+        print("# Synthetic Twitter data set", file=f)
+        print("# Generated with synth_twit.py with following parameters:",
+            file=f)
+        print("# num_nodes: {}".format(num_nodes), file=f)
+        for var,val in saved_args.items():
+            print("# {}: {}".format(var,val), file=f)
+        print("created_at,id,text,user_screen_name", file=f)
+
         # Create an ER random graph.
+        logging.info("Creating graph...")
         pre_epoch_graph = ig.Graph.Erdos_Renyi(num_nodes, p, directed=True)
         pre_epoch_graph.es['color'] = 'black'
+        pre_epoch_graph.vs['name'] = [ ''.join(np.random.choice(
+                list(string.ascii_lowercase + string.digits),
+                size=screenname_length))
+            for _ in range(num_nodes) ]
 
-
+        seven_neighbors = pre_epoch_graph.neighbors(7, mode="in")
+        print("Right now, {} (7) is only receiving influence from: {}".format(
+            pre_epoch_graph.vs[7]['name'], seven_neighbors))
 
         # 1. Simulate the time before the epoch. For each day, perform some
         # number of actions with a node (chosen with replacement).
         record_n_days_worth_of_activity(pre_epoch_graph, num_days_before,
-            num_node_activities_per_day, f)
+            num_node_activities_per_day, dt.datetime.today(), f)
 
         # 2. Make mid-epoch changes. For each connection that exists in the
         # pre-epoch graph, sever it with some probability based on the
@@ -85,22 +138,30 @@ def synth_twit(
         # passed.
         post_epoch_graph = pre_epoch_graph.copy()
         for node in range(num_nodes):
-            these_followees = set(pre_epoch_graph.neighbors(node, mode="out"))
+            these_followees = set(post_epoch_graph.neighbors(node, mode="in"))
             these_non_followees = set(range(num_nodes)) - {node} - \
                 these_followees
             for followee in these_followees:
-                print("Considering severing {}->{}".format(node,followee))
+                logging.debug("Considering severing {}<-{}".format(
+                                                            node,followee))
             for non_followee in these_non_followees:
-                print("Considering adding {}->{}".format(node,non_followee))
+                logging.debug("Considering adding {}<-{}".format(
+                                                        node,non_followee))
                 if (np.random.uniform() < adder(
                                     post_epoch_graph,node,non_followee)):
-                    post_epoch_graph.add_edge(node, non_followee, 
+                    if node == 7:
+                        logging.error("@{} (7) now follows @{} ({})".format(
+                            post_epoch_graph.vs[node]['name'],
+                            post_epoch_graph.vs[non_followee]['name'],
+                            non_followee))
+                    post_epoch_graph.add_edge(non_followee, node,
                                                         color='red',width=2)
             
         # 3. Simulate the time after the epoch. For each day, perform some
         # number of actions with a node (chosen with replacement).
         record_n_days_worth_of_activity(post_epoch_graph, num_days_after,
-            num_node_activities_per_day, f)
+            num_node_activities_per_day, dt.datetime.today() + 
+                dt.timedelta(days=num_days_before), f)
 
         if plot:
             style = { 'vertex_color': 'lightblue' }
@@ -112,11 +173,12 @@ def synth_twit(
 
 
 if __name__ == "__main__":
-    print("Running...")
+    logging.getLogger().setLevel(logging.INFO)
+    logging.critical("Running...")
     synth_twit(num_nodes=20, p=.1,
-        num_days_before=5,
-        num_days_after=5,
-        num_node_activities_per_day=2,
+        screenname_length=5,
+        num_days_before=30,
+        num_days_after=30,
+        num_node_activities_per_day=30,
         adder=shortest_path_length_adder,
         remover=None)
-        

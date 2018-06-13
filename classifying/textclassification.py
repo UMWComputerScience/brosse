346d7b89d9d133b71af0f2b739303d355ed10028
Anerakk <brycjand@yahoo.com>
2018-06-13 16:38:07 -0400

    Should be done. Ran into Error

--- classifying/textclassification.py
+++ classifying/textclassification.py
@@ -9,8 +9,10 @@ Created on Wed Jun  6 14:34:15 2018
 import nltk
 from nltk.corpus import stopwords
 import os
-import operator
+from collections import Counter
 from nltk.corpus import PlaintextCorpusReader
+import operator
+import random
 stopwords = set(stopwords.words('english'))
 corpus_root = '/Users/bryceanderson/Desktop/brosse/classifying'
 rapCorpus = PlaintextCorpusReader(corpus_root, ['rap1.txt', 'rap2.txt','rap3.txt','rap4.txt','rap5.txt'])
@@ -65,18 +67,6 @@ def readlyrics(genre, emptylist):
             with open(file, 'r') as q:
                 song += q.read()
     return song
-#%%
-#doc = open('raplyrics.txt','r')
-#rockdoc = open('rocklyrics.txt','r')
-#raw=doc.read()
-#rockraw = rockdoc.read()
-#raw = prettify(raw)
-#rockraw = prettify(rockraw)
-#rapfreq = prep(raw, stopwords)
-#rockfreq = prep(rockraw, stopwords)
-#Clean set of corpus words
-rapWords = set(rapCorpus.words())
-rockWords = set(rockCorpus.words())
 def cleanSet(aset):
     badwords = []
     temp = aset
@@ -87,47 +77,48 @@ def cleanSet(aset):
     for word in badwords:
         temp.remove(word)
     return temp
-rapWords = cleanSet(rapWords)
-rockWords = cleanSet(rockWords)
-rap_files, r_files = [], []
-raps = readlyrics('rap', rap_files)
-rap_text = readlyrics('rap', r_files)
-raps, rap_text = prettify(raps), prettify(rap_text)
-raps = prep(raps, stopwords)
-rock_files = []
-rock = readlyrics('rock', rock_files)
-rock = prettify(rock)
-rock = prep(rock, stopwords)
-#Combine the sets of vocabulary then do the sorting stuff. Text object any particularly reason.
-all_words = raps + rock
-all_words = sorted(all_words, reverse=True, key=operator.itemgetter(0))
-rap_features = list(raps)[:75]
-rock_features = list(rock)[:75]
-word_features = list(all_words)[:100]
+#%%
+#doc = open('raplyrics.txt','r')
+#rockdoc = open('rocklyrics.txt','r')
+#raw=doc.read()
+#rockraw = rockdoc.read()
+#raw = prettify(raw)
+#rockraw = prettify(rockraw)
+#rapfreq = prep(raw, stopwords)
+#rockfreq = prep(rockraw, stopwords)
+#Want a list of strings containing the lyrics to each song (by genre)
+#Want a list of Text objects that contain each song (by genre)
+def getLyrics(Corpus):
+    lyrics = []
+    text = []
+    for file in Corpus.fileids():
+        temp = ""
+        temp += rapCorpus.raw(file)
+        temp = prettify(temp)
+        lyrics.append(temp)
+        text.append(nltk.Text(temp.split(" ")))
+    return lyrics, text
+rapLyrics, rapText = getLyrics(rapCorpus)
+rockLyrics, rockText = getLyrics(rockCorpus)
+allWords = Counter(nltk.word_tokenize(" ".join(rapLyrics))) + Counter(nltk.word_tokenize(" ".join(rockLyrics)))
+sortVocab = sorted(allWords.items(), reverse=True, key=operator.itemgetter(1))
+word_features = list(sortVocab)[:100]
 
+# For every word in the features list check to see if it exist in the song label (True || False)w
 def song_features(text):
     words = set(text)
-    print(words)
     features = {}
     for word in word_features:
         features["has({})".format(word[0])] = [word[0] in words]
     return features
 
-#featuresets = [(song_features(r), 'rap') for r in ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# For every song in ___Text run song_features and create a tuple with those features
+featuresets = [(song_features(r), 'rap') for r in rapText]
+featuresets += [(song_features(r), 'rock') for r in rockText]
+random.shuffle(featuresets)
+ntrain = int(len(featuresets) * .7)
+trainset, testest = featuresets[:ntrain], featuresets[ntrain:]
+classifie = nltk.NaiveBayesClassifier.train(trainset)
 
 
 

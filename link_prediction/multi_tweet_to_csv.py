31998d93585f2c0d1f076489507060ef1b56d441
HCrosse <HCrosse@users.noreply.github.com>
2018-07-02 14:25:57 -0400

    Changed a print statement to an os.system.

--- link_prediction/multi_tweet_to_csv.py
+++ link_prediction/multi_tweet_to_csv.py
@@ -3,6 +3,6 @@ import os
 directory = "/data/Tweets/JSON"
 
 for f in os.listdir(directory):
-    print("nohup python3 tweet_to_csv_2.py " + f + " &")
+    os.system("nohup python3 tweet_to_csv_2.py " + f + " &")
 
 print('Processes started')

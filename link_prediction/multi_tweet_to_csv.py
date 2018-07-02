ab363501c505d3d460eef51233ec799960e497a7
HCrosse <HCrosse@users.noreply.github.com>
2018-07-02 14:36:32 -0400

    Changed subprocess stuff.

--- link_prediction/multi_tweet_to_csv.py
+++ link_prediction/multi_tweet_to_csv.py
@@ -4,6 +4,6 @@ import subprocess
 directory = "/data/Tweets/JSON"
 
 for f in os.listdir(directory):
-    subprocess.call("nohup python3 tweet_to_csv_2.py " + f + " &")
+    subprocess.run("nohup python3 tweet_to_csv_2.py " + f + " &")
 
 print('Processes started')

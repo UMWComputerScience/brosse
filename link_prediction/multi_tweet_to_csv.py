ea0ede0fb2318a8c3e1b574f8bebcb966cd69d77
HCrosse <HCrosse@users.noreply.github.com>
2018-07-02 14:28:27 -0400

    Shifted from os.system to subprocesses.

--- link_prediction/multi_tweet_to_csv.py
+++ link_prediction/multi_tweet_to_csv.py
@@ -1,8 +1,9 @@
 import os
+import subprocess
 
 directory = "/data/Tweets/JSON"
 
 for f in os.listdir(directory):
-    os.system("nohup python3 tweet_to_csv_2.py " + f + " &")
+    subprocess.call("nohup python3 tweet_to_csv_2.py " + f + " &")
 
 print('Processes started')

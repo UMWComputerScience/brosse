b850828b61f3d9b77bda599535c86d1a9895589f
Anerakk <brycjand@yahoo.com>
2018-07-06 17:23:49 -0400

    More changes

--- database.py
+++ database.py
@@ -4,35 +4,44 @@
 Created on Tue Jul  3 13:50:34 2018
 
 @author: bryceanderson
-"""
-"""
+
+
 To-Do:
     - add some sort of tracking progress system
     - set up skeleton code for database
-    """
+"""
 import psycopg2
 import nltk
-conn = psycopg2.connect(host="", database="tweets", user="postgres") #Need database name and rest or params
+conn = psycopg2.connect(dbname="brosse_test", user="banders6") #Need database name and rest or params
 
 
 
-def get_Tweets():
-    cur = conn.cursor()
-    cur.execute("Select UserID from Users")
-    userID = cur.fetchone()
+def create_Labels(): #<pass in a cursor maybe>
+    user_cur = conn.cursor()
+    user_cur = user_cur.execute("Select userid from temp_sers")
+    userID = user_cur.fetchone()
     """Loop through Users table collecting the User ID"""
     while userID is not None:
-        """Look at Index table and pull tweets rows for userID"""
-        cur.execute("Select Row from Index")
-        rows = cur.fetchall()
+        tweet_cur = conn.cursor()
+        tweet_cur.execute("Select Text from Tweets where userid="+str(userID[0])+"")
+        rows = tweet_cur.fetchall()
         text = ""
         for row in rows:
-            cur.execute("Select Text from Tweets where userID=UserID limit 1 offset row-1")
-            text += cur.fetchone()
+            text += row[0]
         """Do text stuff"""
-    text = prettify(text)
-    text = noHandles(text)
-    text = noStopWords(text)
-    text = nltk.Text(nltk.word_tokenize(text))
-    textFeatures = getFeatures(text)
-    prob
+        print("For user {}".format(userID[0]) + ": " + text)
+        userID = user_cur.fetchone()
+#    text = prettify(text)                   #Make a module file for these methods
+#    text = noHandles(text)                                  #<----
+#    text = noStopWords(text)                                #<----
+#    text = sorted(nltk.FreqDist(nltk.Text(nltk.word_tokenize(text))))
+#    textFeatures = getFeatures(text)                        #<----
+#    Dem = classifier.prob_classify(textFeatures).prob('D')
+#    Rep = classifier.prob_classify(textFeatures).prob('R')
+#    result = conn.cursor()
+#    if Dem > Rep:
+#         #Send Dem to table
+#        result.execute("Update temp_users set party={}".format(Dem) + " where userid={}".format(userID))
+#    else:  #Rep > Dem
+#        result.execute("Update temp_users set party={}".format(Rep) + " where userid={}".format(userID))
+#Send Rep to table
